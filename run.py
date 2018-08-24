87acbf359de3660ec9db28bf6ea6f39794a2005c
dk-denno <dennisveer27@gmail.com>
2018-08-24 11:37:16 +0300

    added run.py

---
+++ run.py
@@ -0,0 +1,27 @@
+from accounts import Accounts
+from passwords import Password
+
+    def create_account(first_name, last_name, user_name, password):
+        accounts = Accounts('dennis', 'Kamau', 'DK-denno', '1234567890')
+        return account
+
+    def save_account(accounts):
+        accounts.save_account()
+
+    def delete_account(accounts):
+        accounts.delete_account()
+
+    def find_accounts(user_name):
+        return Accounts.find_by_user_name(user_name)
+
+    def isexist(username):
+        return Accounts.account_exists(user_name)
+
+    def display_accounts()
+        return Accounts.display_accounts()
+
+    def create_page(page, password):
+        passwords = Password('facebook', '12345')
+        return passwords
+
+    def 
